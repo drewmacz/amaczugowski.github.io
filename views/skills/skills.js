8d13f5c53c67c62e7fa3048092eb5503e87f0b80
amaczugowski <amaczugowski@gmail.com>
2016-07-05 19:08:42 -0400

    skill sort is now improved and added favicon

--- views/skills/skills.js
+++ views/skills/skills.js
@@ -4,17 +4,32 @@
 var app = angular.module('skills', ['myApp']);
 
 // Controller for the skills view
-app.controller('skillsCtrl', function($scope, shared) {
+app.controller('skillsCtrl', function($scope, $filter,  shared) {
     $scope.searchText = '';
+
     $scope.languages = ['Java', 'Javascript', 'Python', 'Groovy', 'C', 'C#', 'MatLab', 'Lua', 'HTML'];
     $scope.frameworks = ['AngularJS', 'Node.js', 'Express', 'Grails', 'Foundation', 'Materialize', 'Mongoose'];
     $scope.tools = ['Eclipse', 'IntelliJ', 'Visual Studio', 'Xcode', 'Android Studio', 'Sublime', 'Atom', 'Nodepad++', 'GNU', 'Vim', 'NPM', 'Bower', 'Gradle', 'MongoDB'];
-    $scope.os = ['Android', 'OS X', 'Windows 7', 'Windows 8', 'Windows 10', 'Linux', 'Redhat', 'CentOS', 'Debian', 'Ubuntu'];
+    $scope.oss = ['Android', 'OS X', 'Windows 7', 'Windows 8', 'Windows 10', 'Linux', 'Redhat', 'CentOS', 'Debian', 'Ubuntu'];
     $scope.skills = ['JUnit', 'REST API', 'Git', 'Version Control', 'GitHub', 'GitLab', 'JSON', 'XML', 'Object Oriented Principles (OOP)', 'Object Oriented Design (OOD)', 'UML', 'Agile', 'MEAN Stack Development', 'Full Stack Development'];
 
+    /**
+     * Called on page load.
+     */
     $scope.pageInit = function() {
         shared.about = false;
         shared.skills = true;
+    };
+
+    /**
+     * Checks if an array contains any items that contain
+     * searchText. Returns true if at least one item contains
+     * it.
+     * @param array
+     * @returns {boolean}
+     */
+    $scope.hasMatches = function(array) {
+        return $filter('filter')(array, $scope.searchText).length > 0;
     };
 
     $scope.pageInit();
