995fb35e8fd95619b102bf0babff5cb2b7c22403
Andrew Maczugowski <amaczugowski@gmail.com>
2016-07-28 16:52:40 -0400

    added link to contact at the bottom of skills

--- views/skills/skills.js
+++ views/skills/skills.js
@@ -11,7 +11,7 @@ app.controller('skillsCtrl', function($scope, $filter,  shared) {
     $scope.frameworks = ['AngularJS', 'Node.js', 'Express', 'Grails', 'Foundation', 'Materialize', 'Mongoose'];
     $scope.tools = ['Eclipse', 'IntelliJ', 'Visual Studio', 'Xcode', 'Android Studio', 'Sublime', 'Atom', 'Nodepad++', 'GNU', 'Vim', 'NPM', 'Bower', 'Gradle', 'Gulp', 'MongoDB', 'PuTTY'];
     $scope.oss = ['Android', 'OS X', 'Windows 7', 'Windows 8', 'Windows 10', 'Linux', 'Redhat', 'CentOS', 'Debian', 'Ubuntu'];
-    $scope.skills = ['JUnit', 'REST API', 'Git', 'Version Control', 'GitHub', 'GitLab', 'JSON', 'XML', 'Object Oriented Principles (OOP)', 'Object Oriented Design (OOD)', 'UML', 'Agile', 'MEAN Stack Development', 'Full Stack Development'];
+    $scope.skills = ['JUnit', 'REST API', 'Git', 'Version Control', 'GitHub', 'GitLab', 'JSON', 'XML', 'Object Oriented Principles (OOP)', 'Object Oriented Design (OOD)', 'MEAN Stack Development', 'Full Stack Development', 'UML', 'Agile'];
 
     /**
      * Called on page load.
